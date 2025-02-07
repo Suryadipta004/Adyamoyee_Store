f836e3fc4b364dfa38d412496985402838158cc2
Suryadipta004 <suryadiptadas.2020@gmail.com>
2025-02-07 18:24:54 +0530

    .

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,43 +1,78 @@
 import React from 'react'
 import logo from '../assets/logo.png'
-import { Link } from 'react-router-dom'
+import { Link, useLocation,useNavigate } from 'react-router-dom'
 import Search from './Search'
+import { FaRegCircleUser } from "react-icons/fa6"
+import useMobile from '../hooks/useMobile';
+import { BsCart4 } from "react-icons/bs";
 
 const Header = () => {
+    const [isMobile] = useMobile() 
+    const location = useLocation()
+    const navigateto = useNavigate()
+    const isSearchPage = location.pathname === "/search"
+    const redirectToLoginPage=() =>{
+        navigateto("/login")
+    }
+
   return (
     <header className='h-24 lg:h-20 lg:shadow-md sticky top-0 z-40 flex flex-col justify-center gap-1 bg-white'>
-            <div className='container mx-auto flex items-center px-2 justify-between'>
-                            {/**logo */}
-                            <div className='h-full'>
-                                <Link to={"/"} className='h-full flex justify-center items-center'>
-                                    <img 
-                                        src={logo}
-                                        width={170}
-                                        height={60}
-                                        alt='logo'
-                                        className='hidden lg:block'
-                                    />
-                                    <img 
-                                        src={logo}
-                                        width={120}
-                                        height={60}
-                                        alt='logo'
-                                        className='lg:hidden'
-                                    />
-                                </Link>
-                            </div>
+        {
+            !(isSearchPage && isMobile) && (
+                <div className='container mx-auto flex items-center px-2 justify-between'>
+                    {/**logo */}
+                    <div className='h-full'>
+                        <Link to={"/"} className='h-full flex justify-center items-center'>
+                            <img 
+                                src={logo}
+                                width={170}
+                                height={60}
+                                alt='logo'
+                                className='hidden lg:block'
+                            />
+                            <img 
+                                src={logo}
+                                width={120}
+                                height={60}
+                                alt='logo'
+                                className='lg:hidden'
+                            />
+                        </Link>
+                    </div>
 
-                            {/**Search */}
-                            <div>
-                                <Search/>
-                            </div>
+                    {/**Search */}
+                    <div className='hidden lg:block'>
+                        <Search/>
+                    </div>
 
-                            {/**login and my cart */}
-                            <div>
-                                Login and My Cart
-                            </div>
-                    
-            </div>
+                    {/**login and my cart */}
+                    <div className=''>
+                        <button className='text-neutral-600 lg:hidden'>
+                            <FaRegCircleUser size={26}/>
+                        </button>
+
+                        {/* dekstop */}
+                        <div className='hidden lg:flex  items-center gap-10'>
+                            <button onClick={redirectToLoginPage} className='text-lg px-2'> Login</button>
+                            <button className='flex items-center gap-2 bg-green-800 hover:bg-green-700 px-3 py-2 rounded text-white'>
+                                {/* add to card icons */}
+                                <div className='animate-bounce'>
+                                    <BsCart4 size={26}/>
+                                </div>
+                                <div className='font-semibold text-sm'>
+                                    <p>My Cart</p>
+                                </div>
+                            </button>
+                        </div>
+                    </div>
+                
+                </div>
+            )
+        }
+        
+        <div className='container mx-auto px-2 lg:hidden'>
+            <Search/>
+        </div>
     </header>
 )
 }
--- client/src/route/index.jsx
+++ client/src/route/index.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home";
+import Search from "../components/SearchPage";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 const router = createBrowserRouter([
     {
         path: "/",
@@ -9,6 +12,18 @@ const router = createBrowserRouter([
             {
                 path: "",
                 element: <Home/>
+            },
+            {
+                path: "search",
+                element: <Search/>
+            },
+            {
+                path: "login",
+                element: <Login/>
+            },
+            {
+                path: "register",
+                element: <Register/>
             }
         ]
     }
